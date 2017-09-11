807c1869997a19abdcb3c6c4a056fd69bb262113
jasmine Magdi <jesojolay@yahoo.com>
2017-09-11 11:47:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ function placeOrder(cardNumber) {
         console.log("Sorry, we don't have a credit card on file for you.");
     } else {
         console.log(`Your total cost is $${sum}, which will be charged to the card ${cardNumber}.`);
-        for (var i = 0; i < cart.length; i++) {
+        for (var i = 0; i <= cart.length; i++) {
             cart.pop();
         }
     }
