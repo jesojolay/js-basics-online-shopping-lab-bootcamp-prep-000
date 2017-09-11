e265f8eaebaa5f2db552f95aa719c74f5603dfbb
jasmine Magdi <jesojolay@yahoo.com>
2017-09-11 11:38:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -92,10 +92,10 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
     // write your code here
+    total();
     if (cardNumber === undefined) {
         console.log("Sorry, we don't have a credit card on file for you.");
     }else{
-        total();
         console.log(`Your total cost is $${total}, which will be charged to the card ${cardNumber}.`);
     }
 }
