7ad483f48866c3bcc8ea7c2a3c15f2b120efc019
jasmine Magdi <jesojolay@yahoo.com>
2017-09-11 11:27:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -79,6 +79,7 @@ function removeFromCart(item) {
     }for (var i = 0; i < cart.length; i++) {
         if (cart[i].hasOwnProperty(item)) {
              cart.splice(i,1);
+             var falsi = true;
             return cart;
         }
     }
