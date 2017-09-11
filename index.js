8007ba02977c17c64dfae1ae83a3312fb676b16e
jasmine Magdi <jesojolay@yahoo.com>
2017-09-11 11:14:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -79,6 +79,8 @@ function removeFromCart(item) {
         if (cart[i].hasOwnProperty(item)) {
              cart.splice(i,1);
             return cart;
+        }else{
+            console.log("That item is not in your cart.");
         }
     }
 
